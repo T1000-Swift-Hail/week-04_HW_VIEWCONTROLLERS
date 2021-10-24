26dbbe17813ba398d2971a91803bd4b382cea69e
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-10-24 18:15:19 +0300

    Done HW

---
+++ Show My Luck Show My Luck Show My Luck  Show My Luck/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Show My Luck Show My Luck Show My Luck  Show My Luck
+//
+//  Created by Munira abdullah on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLuck : UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        myLuck.textColor = UIColor.purple
+        UIFont.systemFont(ofSize: 20)
+        myLuck.numberOfLines = 4
+        myLuck.lineBreakMode = .byWordWrapping
+        
+    }
+    var luckPhrases = ["You will be the best SWIFT developer", "You look great !", "Wonderful things are ahead of you", "Use your imagination more", "A lot of people depend on you. You are up to it!"]
+    @IBAction func showMyLuck (sender: UIButton){
+        
+        let random = Int.random(in: 0..<luckPhrases.count)
+        myLuck.text = "\(luckPhrases[random])"
+       
+    }
+
+}
+
