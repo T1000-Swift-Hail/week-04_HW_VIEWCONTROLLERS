4fde47518d335c6b19627f944d3b3e268864a028
Huda N S <hnsa5@MacBook-Air-Huda.local>
2021-10-24 16:25:33 +0300

    done

---
+++ MyLuck/MyLuck/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MyLuck
+//
+//  Created by Huda N S on 18/03/1443 AH.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    var luckPhrases = ["You will be the best SWIFT developer", "You look great !", "Wonderful things are ahead of you", "Use your imagination more", "A lot of people depend on you. You are up to it!"]
+
+    
+    @IBOutlet weak var myLuck: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        myLuck.textColor = .purple
+        UIFont.systemFont(ofSize: 20 )
+        myLuck.numberOfLines = 4
+        myLuck.lineBreakMode = .byWordWrapping
+}
+    
+    @IBAction func showMyLuck(_ sender: Any) {
+        let random = Int.random(in: 0..<luckPhrases.count)
+        myLuck.text = luckPhrases[random]
+
+    }
+}
