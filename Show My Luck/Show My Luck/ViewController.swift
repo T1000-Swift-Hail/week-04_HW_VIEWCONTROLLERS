d166e20df84884d110caadabd75148baf02b13c0
monaaleid <92249905+monaaleid@users.noreply.github.com>
2021-10-24 21:07:01 +0300

    Done

---
+++ Show My Luck/Show My Luck/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Show My Luck
+//
+//  Created by mona aleid on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var luckPhrases = ["You will be the best SWIFT developer", "You look great !", "Wonderful things are ahead of you", "Use your imagination more", "A lot of people depend on you. You are up to it!"]
+    
+    
+    @IBOutlet weak var myLuck: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myLuck.textColor = .purple
+        UIFont.systemFont(ofSize: 20)
+        myLuck.numberOfLines = 4
+        myLuck.lineBreakMode = .byWordWrapping
+    }
+
+    @IBAction func showMyLuck(_ sender: Any) {
+   
+    let random = Int.random(in: 0..<luckPhrases.count )
+    myLuck.text = luckPhrases[random]
+    
+}
+}
+
+
