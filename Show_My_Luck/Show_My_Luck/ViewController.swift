456785afc1dac14b6458cb6dcfdb471225cb8881
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-24 17:02:33 +0300

    The HW VC it's Done

---
+++ Show_My_Luck/Show_My_Luck/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Show_My_Luck
+//
+//  Created by MACBOOK on 18/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var MyLuck: UILabel!
+    
+    var luckPhrases = ["You will be the best SWIFT developer", "You look great !", "Wonderful things are ahead of you", "Use your imagination more", "A lot of people depend on you. You are up to it!"]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        MyLuck.textColor = .purple
+        MyLuck.font = .systemFont(ofSize: 20)
+        MyLuck.numberOfLines = 4
+        MyLuck.lineBreakMode = .byWordWrapping
+        
+     
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ShowMyLuck(_ sender: Any) {
+        
+        let random = Int.random(in: 0..<luckPhrases.count)
+        MyLuck.text = luckPhrases[random]
+    }
+ 
+}
+
