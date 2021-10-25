42f7406aee20d386110f9458da3e4f867afdc879
mac <mac@MacBook-Pro-alkhas-b-mac.local>
2021-10-25 15:57:58 +0300

    again

--- show-My-liuck/show-My-liuck/ViewController.swift
+++ show-My-liuck/show-My-liuck/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var luck: UIButton!
+    @IBOutlet weak var myLuck: UILabel!
+   let luckPhrases = ["You will be the best SWIFT developer", "You look great !", "Wonderful things are ahead of you", "Use your imagination more", "A lot of people depend on you. You are up to it!"]
+    
+  
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            luck.layer.cornerRadius = 16
+            myLuck.textColor = .purple
+            myLuck.text = "MyLuck"
+            myLuck.font = UIFont.systemFont(ofSize: 20)
+            myLuck.lineBreakMode = .byWordWrapping
     }
 
-
+    @IBAction func ShowMyLiuck(_ sender: Any) {
+        
+        let random = Int.random(in: 0..<luckPhrases.count)
+        myLuck.text =  "\(luckPhrases[random])"
+    }
+    
 }
 
