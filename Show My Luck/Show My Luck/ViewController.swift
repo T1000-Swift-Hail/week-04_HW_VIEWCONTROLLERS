f19d5f866d693b7f6cacf93f8749f73a772dacdf
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-10-25 13:44:12 +0300

    Done

--- Show My Luck/Show My Luck/ViewController.swift
+++ Show My Luck/Show My Luck/ViewController.swift
@@ -9,24 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var ourLabel: UILabel!
+   
     
-    @IBOutlet weak var myLuck: UIButton!
+    @IBOutlet weak var myLock: UILabel!
     
     override func viewDidLoad() {
+        myLock.textColor = .purple
+        myLock.font = UIFont.systemFont(ofSize:20)
+        myLock.numberOfLines = 4
+        myLock.lineBreakMode = .byWordWrapping
         super.viewDidLoad()
-        
-        myLuck.setTitleColor(.purple, for: .normal)
-        ourLabel.font = UIFont.systemFont(ofSize:20)
-        ourLabel.numberOfLines = 4
-        ourLabel.lineBreakMode = .byWordWrapping
+
     }
     
     var luckPhrases = ["You will be the best SWIFT developer", "You look great !", "Wonderful things are ahead of you", "Use your imagination more", "A lot of people depend on you. You are up to it!"]
     
     @IBAction func showMyLuck(_ sender: UIButton) {
         let random1 = Int.random(in:0..<luckPhrases.count)
-        ourLabel.text = luckPhrases[random1]
+        myLock.text = luckPhrases[random1]
     }
     
     
