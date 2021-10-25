31228428dea8478edc8a5693830924fb964c3498
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-10-25 09:18:01 +0300

    the correct one

--- Show My Luck/Show My Luck/ViewController.swift
+++ Show My Luck/Show My Luck/ViewController.swift
@@ -8,25 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-var luckPhrases = ["You will be the best SWIFT developer", "You look great !", "Wonderful things are ahead of you", "Use your imagination more", "A lot of people depend on you. You are up to it!"]
-@IBOutlet weak var myLuck:UILabel!
     
-override func viewDidLoad() {
-super.viewDidLoad()
-
-myLuck.textColor = .purple
-myLuck.font = UIFont.systemFont(ofSize:20)
-myLuck.numberOfLines = 4
-myLuck.lineBreakMode = .byWordWrapping
+    @IBOutlet weak var ourLabel: UILabel!
     
-
+    @IBOutlet weak var myLuck: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myLuck.setTitleColor(.purple, for: .normal)
+        ourLabel.font = UIFont.systemFont(ofSize:20)
+        ourLabel.numberOfLines = 4
+        ourLabel.lineBreakMode = .byWordWrapping
     }
-
-@IBAction func showMyLuck(_sender: UIButton){
-let random1 = Int.random(in:0..<luckPhrases.count)
-myLuck.text = luckPhrases[random1]
+    
+    var luckPhrases = ["You will be the best SWIFT developer", "You look great !", "Wonderful things are ahead of you", "Use your imagination more", "A lot of people depend on you. You are up to it!"]
+    
+    @IBAction func showMyLuck(_ sender: UIButton) {
+        let random1 = Int.random(in:0..<luckPhrases.count)
+        ourLabel.text = luckPhrases[random1]
     }
-
+    
+    
+    
 }
 
